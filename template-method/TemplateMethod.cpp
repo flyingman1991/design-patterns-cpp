4e7e41c861fdc110404dc5f0c398a72fefbef0ea
Harry <flyingman1991@outlook.com>
2022-12-03 16:37:54 +0800

    add uml diagram for template pattern.

--- template-method/TemplateMethod.cpp
+++ template-method/TemplateMethod.cpp
@@ -17,20 +17,22 @@
 class AbstractClass
 {
 public:
-  virtual ~AbstractClass() {}
-  
-  void templateMethod()
-  {
+    virtual ~AbstractClass() {}
+
+    // fixed process
+    void templateMethod()
+    {
+      // ...
+      primitiveOperation1();
+      // ...
+      primitiveOperation2();
+      // ...
+    }
+
+    // different kinds of operations
+    virtual void primitiveOperation1() = 0;
+    virtual void primitiveOperation2() = 0;
     // ...
-    primitiveOperation1();
-    // ...
-    primitiveOperation2();
-    // ...
-  }
-  
-  virtual void primitiveOperation1() = 0;
-  virtual void primitiveOperation2() = 0;
-  // ...
 };
 
 /*
@@ -42,28 +44,28 @@ public:
 class ConcreteClass : public AbstractClass
 {
 public:
-  ~ConcreteClass() {}
-  
-  void primitiveOperation1()
-  {
-    std::cout << "Primitive operation 1" << std::endl;
+    ~ConcreteClass() {}
+
+    void primitiveOperation1()
+    {
+      std::cout << "Primitive operation 1" << std::endl;
+      // ...
+    }
+
+    void primitiveOperation2()
+    {
+      std::cout << "Primitive operation 2" << std::endl;
+      // ...
+    }
     // ...
-  }
-  
-  void primitiveOperation2()
-  {
-    std::cout << "Primitive operation 2" << std::endl;
-    // ...
-  }
-  // ...
 };
 
 
 int main()
 {
-  AbstractClass *tm = new ConcreteClass;
-  tm->templateMethod();
-  
-  delete tm;
-  return 0;
+    AbstractClass *tm = new ConcreteClass;
+    tm->templateMethod();
+
+    delete tm;
+    return 0;
 }
